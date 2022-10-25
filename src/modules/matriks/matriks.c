572507e7d2d902f63117791694529397f493229e
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2022-10-26 00:52:59 +0700

    Adding ADT Matriks

--- src/modules/matriks/matriks.c
+++ src/modules/matriks/matriks.c
@@ -1,58 +1,60 @@
-/* ********** Definisi TYPE Matrix dengan Index dan elemen integer ********** */
+// NIM : 13521156
+// Nama : Brigita Tri Carolina
+// Tanggal : 18 September 2022
+// Topik : ADT Matrix
+// Deskripsi : Program berisi definisi dari fungsi-fungsi pada matrix.h
 
-#ifndef MATRIX_H
-#define MATRIX_H
-
-#include "../boolean/boolean.h"
-
-/* Ukuran maksimum baris dan kolom */
-#define ROW_CAP 100
-#define COL_CAP 100
-
-typedef int IdxType; /* Index baris, kolom */
-typedef char ElType;
-typedef struct
-{
-   ElType mem[ROW_CAP][COL_CAP];
-   int rowEff; /* banyaknya/ukuran baris yg terdefinisi */
-   int colEff; /* banyaknya/ukuran kolom yg terdefinisi */
-} Matrix;
-/* rowEff >= 1 dan colEff >= 1 */
-/* Indeks matriks yang digunakan: [0..ROW_CAP-1][0..COL_CAP-1] */
-/* Memori matriks yang dipakai selalu di "ujung kiri atas" */
+#include "matriks.h"
+#include <stdio.h>
 
 /* ********** DEFINISI PROTOTIPE PRIMITIF ********** */
 /* *** Konstruktor membentuk Matrix *** */
-void createMatrix(int nRows, int nCols, Matrix *m);
+void createMatrix(int nRows, int nCols, Matrix *m) {
 /* Membentuk sebuah Matrix "kosong" yang siap diisi berukuran nRow x nCol di "ujung kiri" memori */
 /* I.S. nRow dan nCol adalah valid untuk memori matriks yang dibuat */
 /* F.S. Matriks m sesuai dengan definisi di atas terbentuk */
-
-/* *** Selektor *** */
-#define ROW_EFF(M) (M).rowEff
-#define COL_EFF(M) (M).colEff
-#define ELMT(M, i, j) (M).mem[(i)][(j)]
+    ROW_EFF(*m) = nRows;
+    COL_EFF(*m) = nCols;
+}
 
 /* *** Selektor "Dunia Matrix" *** */
-boolean isMatrixIdxValid(int i, int j);
+boolean isMatrixIdxValid(int i, int j) { 
 /* Mengirimkan true jika i, j adalah index yang valid untuk matriks apa pun */
+    return i >= 0 && i < ROW_CAP && j >= 0 && j < COL_CAP;
+}
 
 /* *** Selektor: Untuk sebuah matriks m yang terdefinisi: *** */
-IdxType getLastIdxRow(Matrix m);
+IdxType getLastIdxRow(Matrix m) {
 /* Mengirimkan Index baris terbesar m */
-IdxType getLastIdxCol(Matrix m);
+    return ROW_EFF(m) - 1;
+}
+IdxType getLastIdxCol(Matrix m) {
 /* Mengirimkan Index kolom terbesar m */
-boolean isIdxEff(Matrix m, IdxType i, IdxType j);
+    return COL_EFF(m) - 1;
+}
+boolean isIdxEff(Matrix m, IdxType i, IdxType j) {
 /* Mengirimkan true jika i, j adalah Index efektif bagi m */
-ElType getElmtDiagonal(Matrix m, IdxType i);
+    return i >= 0 && i < ROW_EFF(m) && j >= 0 && j < COL_EFF(m);
+}
+ElType getElmtDiagonal(Matrix m, IdxType i) {
 /* Mengirimkan elemen m(i,i) */
+    return ELMT(m, i, i);
+}
 
 /* ********** Assignment  Matrix ********** */
-void copyMatrix(Matrix mIn, Matrix *mOut);
+void copyMatrix(Matrix mIn, Matrix *mOut) {
 /* Melakukan assignment mOut <- mIn */
+    createMatrix(ROW_EFF(mIn), COL_EFF(mIn), mOut);
+    for (int i = 0; i < ROW_EFF(mIn); i++) {
+        for (int j = 0; j < COL_EFF(mIn); j++) {
+            ELMT(*mOut, i, j) = ELMT(mIn, i, j); 
+        }
+    }
+
+}
 
 /* ********** KELOMPOK BACA/TULIS ********** */
-void readMatrix(Matrix *m, int nRow, int nCol);
+void readMatrix(Matrix *m, int nRow, int nCol) {
 /* I.S. isIdxValid(nRow,nCol) */
 /* F.S. m terdefinisi nilai elemen efektifnya, berukuran nRow x nCol */
 /* Proses: Melakukan CreateMatrix(m,nRow,nCol) dan mengisi nilai efektifnya */
@@ -61,9 +63,17 @@ void readMatrix(Matrix *m, int nRow, int nCol);
 ###
 ***
 ###
-*/
+*/  
 /* Pembacaan char tanpa spasi dan setiap barisnya diakhiri enter */
-void displayMatrix(Matrix m);
+    createMatrix(nRow, nCol, m);
+    for (int i = 0; i < nRow; i++) {
+        for (int j = 0; j < nCol; j++) {
+            scanf("\n%c", &ELMT(*m, i, j));
+        }
+    }
+}
+
+void displayMatrix(Matrix m) {
 /* I.S. m terdefinisi */
 /* F.S. Nilai m(i,j) ditulis ke layar per baris per kolom, masing-masing elemen per baris 
    dipisahkan sebuah spasi. Baris terakhir tidak diakhiri dengan newline */
@@ -73,24 +83,54 @@ void displayMatrix(Matrix m);
 # # # 
 # # # 
 */
+    for(int i = 0; i < ROW_EFF(m); i++) {
+        for (int j = 0; j < COL_EFF(m); j++) {
+            if (j == COL_EFF(m) - 1) {
+                printf("%c\n", ELMT(m, i, j));
+            } else {
+                printf("%c ", ELMT(m, i, j));
+            }
+        }
+    }
+}
 
 /* ********** KELOMPOK OPERASI RELASIONAL TERHADAP Matrix ********** */
-boolean isMatrixEqual(Matrix m1, Matrix m2);
+boolean isMatrixEqual(Matrix m1, Matrix m2) {
 /* Mengirimkan true jika m1 = m2, yaitu count(m1) = count(m2) dan */
 /* untuk setiap i,j yang merupakan Index baris dan kolom m1(i,j) = m2(i,j) */
 /* Juga merupakan strong eq karena getLastIdxCol(m1) = getLastIdxCol(m2) */
-boolean isMatrixNotEqual(Matrix m1, Matrix m2);
+    if (countElmt(m1) == countElmt(m2) && getLastIdxCol(m1) == getLastIdxCol(m2)) {
+        for (int i = 0; i < ROW_EFF(m1); i++) {
+            for (int j = 0; j < COL_EFF(m1); j++) {
+                if (ELMT(m1, i, j) != ELMT(m2, i, j)) {
+                    return false;
+                }
+            }
+        }
+    } else {
+        return false;
+    }
+    return true;
+}
+
+boolean isMatrixNotEqual(Matrix m1, Matrix m2) {
 /* Mengirimkan true jika m1 tidak sama dengan m2 */
-boolean isMatrixSizeEqual(Matrix m1, Matrix m2);
+    return !isMatrixEqual(m1, m2);
+}
+boolean isMatrixSizeEqual(Matrix m1, Matrix m2) {
 /* Mengirimkan true jika ukuran efektif matriks m1 sama dengan ukuran efektif m2 */
 /* yaitu RowEff(m1) = RowEff (m2) dan ColEff (m1) = ColEff (m2) */
+    return ROW_EFF(m1) == ROW_EFF(m2) && COL_EFF(m1) == COL_EFF(m2);
+}
 
 /* ********** Operasi lain ********** */
-int countElmt(Matrix m);
+int countElmt(Matrix m) {
 /* Mengirimkan banyaknya elemen m */
+    return ROW_EFF(m)*COL_EFF(m);
+}
 
 /* ********** KELOMPOK TEST TERHADAP Matrix ********** */
-boolean isSquare(Matrix m);
+boolean isSquare(Matrix m) {
 /* Mengirimkan true jika m adalah matriks dg ukuran baris dan kolom sama */
-
-#endif
+    return  ROW_EFF(m) == COL_EFF(m);
+}
