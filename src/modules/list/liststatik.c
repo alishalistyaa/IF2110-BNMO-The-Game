fcc3c13dab4c8022bff407041ce80c0d22dd3be0
[SulthanDA28] <[13521159@std.stei.itb.ac.id]>
2022-10-25 10:08:02 +0700

    commat commit

---
+++ src/modules/list/liststatik.c
@@ -0,0 +1,95 @@
+#include"liststatik.h"
+#include"boolean.h"
+#include<stdio.h>
+void CreateListStatik(ListStatik *l)
+{
+    int i;
+    for (i=0;i<CAPACITY;i++)
+    {
+        ELMT(*l,i)=MARK;
+    }
+}
+int lengthList(ListStatik l)
+{
+    int i = 0;
+    while(ELMT(l,i)!=MARK && i!=CAPACITY)
+    {
+        i++;
+    }
+    return (i);
+}
+boolean isValValid(ListStatik l, ElType val)
+{
+    boolean found = false;
+    int i = 0;
+    while(ELMT(l,i)!=MARK && i!=CAPACITY && !found)
+    {
+        if(ELMT(l,i)==val)
+        {
+            found = true;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return (found);
+}
+boolean isIdxEff(ListStatik l, IdxType i)
+{
+    return(i>=0 && i<lengthList(l));
+}
+boolean isEmpty(ListStatik l)
+{
+    return(lengthList(l)==0);
+}
+boolean isFull(ListStatik l)
+{
+    return(lengthList(l)==CAPACITY);
+}
+void bacaList(ListStatik *l)
+{
+    int n;
+    int i;
+    printf("Masukkan banyak item yang akan dimasukkan ke dalam list = ");
+    scanf("%i",&n);
+    while (n<0 || n>CAPACITY)
+    {
+        printf("Masukkan salah,silahkan input lagi = ");
+        scanf("%i",&n);
+    }
+    CreateListStatik(l);
+    if(n!=0)
+    {
+        for(i=0;i<n;i++)
+        {
+            printf("Masukkan ID makanan/resep %i = \n",(i+1));
+            scanf("%i",&ELMT(*l,i));
+        }
+    }
+}
+void cetakList(ListStatik l)
+{
+    int panjang = lengthList(l);
+    int i;
+    if(panjang == 0)
+    {
+        printf("List kosong");
+    }
+    else
+    {
+        for(i = 0;i<panjang;i++)
+        {
+            printf("%i. %i\n",(i+1),ELMT(l,i));
+        }
+    }
+}
+void CopyList(ListStatik l1,ListStatik *l2)
+{
+    int i = 0;
+    while(ELMT(l1,i)!=MARK && i != CAPACITY)
+    {
+        ELMT(*l2,i) = ELMT(l1,i);
+        i++;
+    }
+}
---
+++ src/modules/list/liststatik.h
@@ -0,0 +1,72 @@
+#ifndef LISTSTATIK_H
+#define LISTSTATIK_H
+
+#include"boolean.h"
+
+/*  Kamus Umum */
+#define CAPACITY 100
+/* Kapasitas penyimpanan */
+#define IDX_MIN 0
+/* Indeks minimum list */
+#define IDX_UNDEF -1
+/* Indeks tak terdefinisi*/
+#define MARK -9999
+/* Nilai elemen tak terdefinisi*/
+
+/* Definisi elemen dan koleksi objek */
+typedef int ElType;  /* type elemen List */
+typedef int IdxType;
+typedef struct {
+   ElType contents[CAPACITY]; /* memori tempat penyimpan elemen (container) */
+} ListStatik;
+
+#define ELMT(l, i) (l).contents[(i)]
+
+//--KONSTRUKTOR--
+
+void CreateListStatik(ListStatik *l);
+/* I.S. l sembarang */
+/* F.S. Terbentuk List l kosong dengan kapasitas CAPACITY */
+/* Proses: Inisialisasi semua elemen List l dengan MARK */
+
+int lengthList(ListStatik l);
+/* Mengirimkan banyaknya elemen efektif List */
+/* Mengirimkan nol jika List kosong */  
+
+boolean isValValid(ListStatik l, ElType val);
+/* Mengirimkan true jika i adalah elemen yang valid dari list */
+
+boolean isIdxEff(ListStatik l, IdxType i);
+/* Mengirimkan true jika i adalah indeks yang terdefinisi utk List l */
+/* yaitu antara 0..length(l)-1 */
+
+boolean isEmpty(ListStatik l);
+/* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
+
+
+boolean isFull(ListStatik l);
+/* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
+
+void bacaList(ListStatik *l);
+/* I.S. l sembarang */
+/* F.S. List l terdefinisi */
+/* Proses: membaca banyaknya elemen l dan mengisi nilainya */
+/* 1. Baca banyaknya elemen diakhiri enter, misalnya n */
+/*    Pembacaan diulangi sampai didapat n yang benar yaitu 0 <= n <= CAPACITY */
+/*    Jika n tidak valid, tidak diberikan pesan kesalahan */
+/* 2. Jika 0 < n <= CAPACITY; Lakukan n kali: 
+          Baca elemen mulai dari indeks 0 satu per satu diakhiri enter */
+/*    Jika n = 0; hanya terbentuk List kosong */
+
+void cetakList(ListStatik l);
+/* Proses : Menuliskan isi List dengan traversal, List ditulis di antara kurung 
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. l boleh kosong */
+/* F.S. Jika l tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika List kosong : menulis [] */
+void CopyList(ListStatik l1,ListStatik *l2);
+/*Menyalin semua elemen di list 1 ke list 2*/
+
+#endif
